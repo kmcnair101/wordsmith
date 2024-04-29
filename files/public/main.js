509549f2728ba47c49f54ae2355357160c1da6d1
Kris McNair <kmcnair_100@outlook.com>
2024-04-29 18:41:03 -0400

    add post request

--- files/public/main.js
+++ files/public/main.js
@@ -1,34 +1,74 @@
 // Information to reach API
-const url = "https://api.datamuse.com/words?sl="
+const dataMuseUrl = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
 
-// Selects page elements
+// Selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // Asynchronous function
-const getSuggestions = () => {
-    const wordQuery = inputField.value;
-    const endpoint = url + wordQuery;
-    fetch(endpoint, {cache: 'no-cache'})
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      }
-      throw new Error('Request failed!');
-    }, networkError => console.log(networkError.message))
-    .then( jsonResponse => {
-        renderResponse(jsonResponse)
-      } )
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${dataMuseUrl}${queryParams}${wordQuery}`;
+  try {
+    const response = await fetch(endpoint, {cache: 'no-cache'});
+    if(response.ok){
+      const jsonResponse = await response.json();
+      renderWordResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
   }
+}
 
-// Clears previous results and display results to webpage
+// Clear previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
   }
   getSuggestions();
-};
+}
 
 submit.addEventListener('click', displaySuggestions);
+
+// information to reach API
+const apiKey = '<Your API Key>';
+const rebrandlyEndpoint = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const shortenButton = document.querySelector('#shorten');
+
+// Asynchronous functions
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(rebrandlyEndpoint, {
+			method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+				'apikey': apiKey
+      }
+    });
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderByteResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
