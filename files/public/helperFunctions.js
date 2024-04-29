510bb905edaa26fcb677ff22f981b802e614c4cf
Kris McNair <kmcnair_100@outlook.com>
2024-04-29 18:46:17 -0400

    Manipulate responseField to render an unformatted response

--- files/public/helperFunctions.js
+++ files/public/helperFunctions.js
@@ -1,5 +1,5 @@
-// Formats response to look presentable on webpage
-const renderResponse = (res) => {
+// Formats Response to look presentable on webpage
+const renderWordResponse = (res) => {
     // Handles if res is falsey
     if(!res){
       console.log(res.status);
@@ -10,11 +10,11 @@ const renderResponse = (res) => {
       return;
     }
   
-    // Creates an empty array to contain the HTML strings
-    let wordList = [];
-    // Loops through the response and caps off at 10
+    // Creates an array to contain the HTML strings
+    let wordList = []
+    // Loops through the response and maxxing out at 10
     for(let i = 0; i < Math.min(res.length, 10); i++){
-      // creating a list of words
+      // Creates a list of words
       wordList.push(`<li>${res[i].word}</li>`);
     }
     // Joins the array of HTML strings into one string
@@ -22,11 +22,11 @@ const renderResponse = (res) => {
   
     // Manipulates responseField to render the modified response
     responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
-    return
+    return;
   }
   
   // Renders response before it is modified
-  const renderRawResponse = (res) => {
+  const renderRawWordResponse = (res) => {
     // Takes the first 10 words from res
     let trimmedResponse = res.slice(0, 10);
     // Manipulates responseField to render the unformatted response
@@ -34,15 +34,40 @@ const renderResponse = (res) => {
   }
   
   // Renders the JSON that was returned when the Promise from fetch resolves.
-  const renderJsonResponse = (res) => {
-    // Creates an empty object to store the JSON in key-value pairs
+  const renderJsonWordResponse = (res) => {
+    // Creating an empty object to store the JSON in key-value pairs
     let rawJson = {};
-    for(let key in res){
-      rawJson[key] = res[key];
+    for(let key in response){
+      rawJson[key] = response[key];
     }
-    // Converts JSON into a string and adding line breaks to make it easier to read
+    // Converting JSON into a string and adding line breaks to make it easier to read
     rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n");
     // Manipulates responseField to show the returned JSON.
     responseField.innerHTML = `<pre>${rawJson}</pre>`;
   }
   
+  // Manipulates responseField to render a formatted and appropriate message
+  const renderByteResponse = (res) => {
+    if(res.errors){
+      // Will change the HTML to show this error message if the response had an error
+      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {
+      // If there was no error, then the HTML will show this message
+      responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+    }
+  }
+  
+  // Manipulates responseField to render an unformatted response
+  const renderRawByteResponse = (res) => {
+    if(res.errors){
+      // Changes the HTML to show this error message if the response had an error
+      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {
+      // Changes the HTML to show this raw response if there was no error
+      // Formats the response into a more organized structure
+      let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+      structuredRes = `<pre>${structuredRes}</pre>`;
+  
+      responseField.innerHTML = `${structuredRes}`;
+    }
+  }
